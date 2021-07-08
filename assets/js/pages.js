1749d38c31f299ba42703869443f018c841636b2
Ax7-cmd <faishal.amrullah@bitcoin.co.id>
2021-07-08 16:24:08 +0700

    update data market cap rank & header menu

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
     $('#fishHolder').fishAnimation();
 
     // load data circulating supply
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
+    var cycXhttp = new XMLHttpRequest();
+    cycXhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            const obj = JSON.parse(xhttp.response);
+            const obj = JSON.parse(cycXhttp.response);
             $('.cyc-value').html(obj.strValue + ' TAD');
             $('.cyc-percent').html(obj.percent + '%');
             $('.cyc-percent-style').attr('style', 'width: ' + obj.percent + '%');
@@ -16,6 +16,19 @@ $(document).ready(function () {
             $('.cyc-update').html('* update, ' + obj.updatedAt);
         }
     };
-    xhttp.open("GET", "//api.npoint.io/f7f14792a99a6209db30", true);
-    xhttp.send();
+    cycXhttp.open("GET", "//api.npoint.io/f7f14792a99a6209db30", true);
+    cycXhttp.send();
+
+    // load market cap rank
+    var mcrXhttp = new XMLHttpRequest();
+    mcrXhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const obj = JSON.parse(mcrXhttp.response);
+            if (obj.market_cap_rank != undefined) {
+                $('.mcr').html('#' + obj.market_cap_rank + ' at Market Cap Rank');
+            }
+        }
+    };
+    mcrXhttp.open("GET", "https://api.coingecko.com/api/v3/coins/tadpole-finance?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false", true);
+    mcrXhttp.send();
 });
